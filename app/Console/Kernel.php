9b9080e2d38f5b59715a6f30d0674958c432c225
miatraxkhalifa <miatraxkhalifa@gmail.com>
2022-05-02 20:56:35 +1000

    with DBBACKUP

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,40 +1,77 @@
 <?php
 
+
+
 namespace App\Console;
 
+
+
 use Illuminate\Console\Scheduling\Schedule;
+
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+
+
 class Kernel extends ConsoleKernel
+
 {
+
     /**
+
      * The Artisan commands provided by your application.
+
      *
+
      * @var array
+
      */
+
     protected $commands = [
-        //
+
+        'App\Console\Commands\DatabaseBackUp'
+
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
-    }
+
 
     /**
-     * Register the commands for the application.
+
+     * Define the application's command schedule.
+
      *
+
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+
      * @return void
+
      */
-    protected function commands()
+
+    protected function schedule(Schedule $schedule)
+
     {
-        $this->load(__DIR__.'/Commands');
+
+        $schedule->command('database:backup')->daily();
+    }
+
+
+
+    /**
+
+     * Register the commands for the application.
+
+     *
+
+     * @return void
+
+     */
+
+    protected function commands()
+
+    {
+
+        $this->load(__DIR__ . '/Commands');
+
+
 
         require base_path('routes/console.php');
     }
